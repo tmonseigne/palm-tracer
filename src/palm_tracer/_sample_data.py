bfa64f8e3eb731696358b19d1959dee182b9a0f5
Thibaut <thibaut.monseigne@u-bordeaux.fr>
2024-12-04 17:19:20 +0100

    :recycle: Apply Coding Rules on template

--- src/palm_tracer/_sample_data.py
+++ src/palm_tracer/_sample_data.py
@@ -13,10 +13,8 @@ import numpy
 
 
 def make_sample_data():
-    """Generates an image"""
-    # Return list of tuples
-    # [(data1, add_image_kwargs1), (data2, add_image_kwargs2)]
-    # Check the documentation for more information about the
-    # add_image_kwargs
-    # https://napari.org/stable/api/napari.Viewer.html#napari.Viewer.add_image
-    return [(numpy.random.rand(512, 512), {})]
+	"""Generates an image"""
+	# Return list of tuples [(data1, add_image_kwargs1), (data2, add_image_kwargs2)]
+	# Check the documentation for more information about the add_image_kwargs
+	# https://napari.org/stable/api/napari.Viewer.html#napari.Viewer.add_image
+	return [(numpy.random.rand(512, 512), {})]
