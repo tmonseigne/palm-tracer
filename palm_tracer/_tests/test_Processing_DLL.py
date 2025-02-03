f0395e56888ee4d5a2cb09aad8da75a1ef900a1e
Thibaut <thibaut.monseigne@u-bordeaux.fr>
2025-02-03 10:43:38 +0100

    :white_check_mark: prepare tests

--- palm_tracer/_tests/test_Processing_DLL.py
+++ palm_tracer/_tests/test_Processing_DLL.py
@@ -3,12 +3,19 @@
 import os
 from pathlib import Path
 
+import pandas as pd
+
 from palm_tracer.Processing import load_dll, run_palm_image_dll, run_palm_stack_dll
 from palm_tracer.Tools import open_tif, print_warning
 
 INPUT_DIR = Path(__file__).parent / "input"
 OUTPUT_DIR = Path(__file__).parent / "output"
 os.makedirs(OUTPUT_DIR, exist_ok=True)  # Créer le dossier de sorties (la première fois, il n'existe pas)
+
+
+##################################################
+def compare_localisations(value: pd.DataFrame, ref: pd.DataFrame, tol: float = 1e-5) -> bool:
+	return value[["X", "Y", "Z"]].sub(ref[["X", "Y", "Z"]]).abs().lt(tol).all().all()
 
 
 ##################################################
@@ -36,8 +43,13 @@ def test_run_palm_image_dll():
 		for plane in range(stack.shape[0]):
 			for gaussian in range(5):
 				localisations = run_palm_image_dll(dll, stack[plane], threshold, watershed, gaussian, sigma, theta, roi)
-				# localisations.to_csv(f"{OUTPUT_DIR}/{file}-{plane}_{threshold}_{watershed}_{gaussian}_{sigma}_{theta}_{roi}.csv", index=False)
+				# localisations.to_csv(f"{OUTPUT_DIR}/{file}-localisations_{plane}_{threshold}_{watershed}_{gaussian}_{sigma}_{theta}_{roi}.csv", index=False)
 				assert len(localisations) > 0, "Aucune localisation trouvé"
+				path = Path(f"{INPUT_DIR}/{file}-localisations_{plane}_{threshold}_{watershed}_{gaussian}_{sigma}_{theta}_{roi}.csv")
+				if path.exists() and path.is_file():
+					ref = pd.read_csv(path)
+					assert compare_localisations(localisations, ref), (f"Test invalide pour les paramètres "
+																	   f"{plane}_{threshold}_{watershed}_{gaussian}_{sigma}_{theta}_{roi}")
 		print_warning("\n====================\nAucune comparaison avec Metamorph dans ce test.\n====================\n")
 	assert True
 
@@ -59,8 +71,13 @@ def test_run_palm_stack_dll():
 		threshold, watershed, sigma, theta, roi = 103.6, True, 1.0, 0.0, 7
 		for gaussian in range(5):
 			localisations = run_palm_stack_dll(dll, stack, threshold, watershed, gaussian, sigma, theta, roi)
-			# localisations.to_csv(f"{OUTPUT_DIR}/{file}-{threshold}_{watershed}_{gaussian}_{sigma}_{theta}_{roi}.csv", index=False)
+			# localisations.to_csv(f"{OUTPUT_DIR}/{file}-localisations_{threshold}_{watershed}_{gaussian}_{sigma}_{theta}_{roi}.csv", index=False)
 			assert len(localisations) > 0, "Aucune localisation trouvé"
+			path = Path(f"{INPUT_DIR}/{file}-localisations_{threshold}_{watershed}_{gaussian}_{sigma}_{theta}_{roi}.csv.csv")
+			if path.exists() and path.is_file():
+				ref = pd.read_csv(path)
+				assert compare_localisations(localisations, ref), (f"Test invalide pour les paramètres "
+																   f"{threshold}_{watershed}_{gaussian}_{sigma}_{theta}_{roi}")
 		print_warning("\n====================\nAucune comparaison avec Metamorph dans ce test.\n====================\n")
 	assert True
 
@@ -89,4 +106,8 @@ def test_run_palm_stack_dll_check_quadrant():
 		assert (localisations.loc[quadrant["Bottom"], 'Y'] >= 128).all(), "Des éléments ont été trouvé dans la zone noire en bas de l'image."
 		assert (localisations.loc[quadrant["Left"], 'X'] <= 128).all(), "Des éléments ont été trouvé dans la zone noire à gauche de l'image."
 		assert (localisations.loc[quadrant["Right"], 'X'] >= 128).all(), "Des éléments ont été trouvé dans la zone noire à droite de l'image."
+		path = Path(f"{INPUT_DIR}/{file}-localisations_{threshold}_{watershed}_0_{sigma}_{theta}_{roi}.csv.csv")
+		if path.exists() and path.is_file():
+			ref = pd.read_csv(path)
+			assert compare_localisations(localisations, ref), "Test invalide pour la vérification des quadrants."
 	assert True
