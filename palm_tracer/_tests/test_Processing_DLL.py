fb77211d40f92253b30c46a526757e9c93d1a9b1
Thibaut <thibaut.monseigne@u-bordeaux.fr>
2025-02-05 10:16:17 +0100

    :construction: fix tests

--- palm_tracer/_tests/test_Processing_DLL.py
+++ palm_tracer/_tests/test_Processing_DLL.py
@@ -12,6 +12,8 @@ INPUT_DIR = Path(__file__).parent / "input"
 OUTPUT_DIR = Path(__file__).parent / "output"
 os.makedirs(OUTPUT_DIR, exist_ok=True)  # Créer le dossier de sorties (la première fois, il n'existe pas)
 
+threshold, watershed, sigma, theta, roi = 103.6, True, 1.0, 0.0, 7
+
 
 ##################################################
 def compare_localisations(value: pd.DataFrame, ref: pd.DataFrame, tol: float = 1e-5) -> bool:
@@ -40,7 +42,6 @@ def test_run_palm_image_dll():
 	else:
 		file = "stack"
 		stack = open_tif(f"{INPUT_DIR}/{file}.tif")
-		threshold, watershed, sigma, theta, roi = 103.6, True, 1.0, 0.0, 7
 		for plane in range(stack.shape[0]):
 			for gaussian in range(5):
 				localisations = run_palm_image_dll(dll, stack[plane], threshold, watershed, gaussian, sigma, theta, roi)
@@ -70,12 +71,11 @@ def test_run_palm_stack_dll():
 	else:
 		file = "stack"
 		stack = open_tif(f"{INPUT_DIR}/{file}.tif")
-		threshold, watershed, sigma, theta, roi = 103.6, True, 1.0, 0.0, 7
 		for gaussian in range(5):
 			localisations = run_palm_stack_dll(dll, stack, threshold, watershed, gaussian, sigma, theta, roi)
 			# localisations.to_csv(f"{OUTPUT_DIR}/{file}-localisations_{threshold}_{watershed}_{gaussian}_{sigma}_{theta}_{roi}.csv", index=False)
 			assert len(localisations) > 0, "Aucune localisation trouvé"
-			path = Path(f"{INPUT_DIR}/{file}-localisations_{threshold}_{watershed}_{gaussian}_{sigma}_{theta}_{roi}.csv.csv")
+			path = Path(f"{INPUT_DIR}/{file}-localisations_{threshold}_{watershed}_{gaussian}_{sigma}_{theta}_{roi}.csv")
 			if path.exists() and path.is_file():
 				ref = pd.read_csv(path)
 				assert compare_localisations(localisations, ref), (f"Test invalide pour les paramètres "
@@ -99,7 +99,6 @@ def test_run_palm_stack_dll_check_quadrant():
 	else:
 		file = "stack_quadrant"
 		stack = open_tif(f"{INPUT_DIR}/{file}.tif")
-		threshold, watershed, sigma, theta, roi = 103.6, False, 1.0, 0.0, 7
 		localisations = run_palm_stack_dll(dll, stack, threshold, watershed, 0, sigma, theta, roi)
 		quadrant = {"Top":    localisations['Plane'].isin([3, 4, 7, 8]),
 					"Bottom": localisations['Plane'].isin([1, 2, 5, 6, 9, 10]),
@@ -109,7 +108,7 @@ def test_run_palm_stack_dll_check_quadrant():
 		assert (localisations.loc[quadrant["Bottom"], 'Y'] >= 128).all(), "Des éléments ont été trouvé dans la zone noire en bas de l'image."
 		assert (localisations.loc[quadrant["Left"], 'X'] <= 128).all(), "Des éléments ont été trouvé dans la zone noire à gauche de l'image."
 		assert (localisations.loc[quadrant["Right"], 'X'] >= 128).all(), "Des éléments ont été trouvé dans la zone noire à droite de l'image."
-		path = Path(f"{INPUT_DIR}/{file}-localisations_{threshold}_{watershed}_0_{sigma}_{theta}_{roi}.csv.csv")
+		path = Path(f"{INPUT_DIR}/{file}-localisations_{threshold}_{watershed}_0_{sigma}_{theta}_{roi}.csv")
 		if path.exists() and path.is_file():
 			ref = pd.read_csv(path)
 			assert compare_localisations(localisations, ref), "Test invalide pour la vérification des quadrants."
