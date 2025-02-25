a7da79476a519663fc96194d8f740191a807548c
Thibaut <thibaut.monseigne@u-bordeaux.fr>
2025-02-25 14:57:37 +0100

    :construction: Histogram

--- palm_tracer/Processing/Visualization.py
+++ palm_tracer/Processing/Visualization.py
@@ -2,7 +2,8 @@
 Module contenant les fonctions de visualization pouyr palm-tracer.
 """
 import numpy as np
-
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 ##################################################
 def hr_visualization(width: int, height: int, ratio: int, points: np.ndarray) -> np.ndarray:
@@ -16,7 +17,7 @@ def hr_visualization(width: int, height: int, ratio: int, points: np.ndarray) ->
 	:return: Nouvelle image.
 	"""
 
-	if ratio <=1: return np.zeros((width, height), dtype=np.uint16)
+	if ratio <= 1: return np.zeros((width, height), dtype=np.uint16)
 	if width < 1 or height < 1: return np.zeros((max(int(width * ratio), 1), max(int(height * ratio), 1)), dtype=np.uint16)
 
 	res = np.zeros((int(width * ratio), int(height * ratio)), dtype=float)
@@ -37,3 +38,40 @@ def hr_visualization(width: int, height: int, ratio: int, points: np.ndarray) ->
 
 	res = res.clip(0, np.iinfo(np.uint16).max)  # Limite les valeurs entre 0 et la valeur maximale possible pour un uint16
 	return np.asarray(res, dtype=np.uint16)		# Forcer le type de l'image en np.uint16
+
+
+##################################################
+def plot_histogram(ax: plt.axes, data: np.ndarray, title: str, limit: bool = True, gaussian: bool = True):
+	"""
+	Trace un histogramme pour les données en entrée en utilisant la méthode des "3 sigma" pour supprimer les valeurs extrêmes.
+	:param ax: Axe sur lequel tracer l'histogramme.
+	:param data: Données à tracer sous forme de tableau numpy.
+	:param title: Titre de l'histogramme.
+	:param limit: Ajoute une limite avec la règle des 3 sigmas.
+	:param gaussian: Trace une gaussienne sur l'histogramme.
+	"""
+	if len(data) == 0: return
+	mu, sigma = np.mean(data), np.std(data)
+	if sigma == 0: return
+
+	# Ajout d'un style avec Seaborn
+	sns.set_style("white")
+
+	# Limite des données avec la règle des 3 Sigmas
+	if limit:
+		limits = [mu - 3 * sigma, mu + 3 * sigma]						 # Limite théoriques des datas
+		data = data[(data >= limits[0]) & (data <= limits[1])]			 # Suppression des datas au dela des limites
+		limits = [max(limits[0], min(data)), min(limits[1], max(data))]  # On resserre les limites autour des datas
+	else:
+		limits = [min(data), max(data)]
+
+	_, _, _ = ax.hist(data, bins=30, alpha=0.75, density=True)
+	ax.set_title(title)
+	ax.set_xlim(limits)
+	ax.set_xlabel("Values")
+	ax.set_ylabel("Density")
+
+	# Ajout d'une courbe gaussienne
+	if gaussian:
+		x = np.linspace(limits[0], limits[1], 100)
+		ax.plot(x, 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(-(x - mu) ** 2 / (2 * sigma ** 2)), linestyle="--")
--- palm_tracer/_tests/test_Processing_Visualization.py
+++ palm_tracer/_tests/test_Processing_Visualization.py
@@ -3,8 +3,9 @@ import os
 from pathlib import Path
 
 import numpy as np
+from matplotlib import pyplot as plt
 
-from palm_tracer.Processing import hr_visualization
+from palm_tracer.Processing import hr_visualization, plot_histogram
 from palm_tracer.Tools.FileIO import save_png
 
 INPUT_DIR = Path(__file__).parent / "input"
@@ -37,3 +38,20 @@ def test_hr_visualization_bad_input():
 	visualization = hr_visualization(SIZE, SIZE, RATIO, np.zeros((2, 2)))
 	assert visualization.shape == (SIZE * RATIO, SIZE * RATIO)
 	assert np.all(visualization == 0)
+
+
+##################################################
+def test_plot_histogram():
+	datas = rng.normal(loc=10, scale=5, size=(int(SIZE * np.sqrt(SIZE)), 1))
+	# Ajouter des aberrations (bruit) aux points
+	aberration = rng.uniform(-5, 5, datas.shape)
+	datas += aberration
+
+	fig, ax = plt.subplots()
+	plot_histogram(ax, datas, "Histogram")
+	fig.savefig(f"{OUTPUT_DIR}/test_plot_histogram.png", bbox_inches="tight")
+	ax.clear()
+
+	plot_histogram(ax, datas, "Histogram", False, False)
+	fig.savefig(f"{OUTPUT_DIR}/test_plot_histogram_without.png", bbox_inches="tight")
+	plt.close(fig)
