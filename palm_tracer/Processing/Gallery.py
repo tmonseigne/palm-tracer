52ff982c84199a090e3f649c277c2b99fb9264fa
Thibaut <thibaut.monseigne@u-bordeaux.fr>
2025-04-28 11:00:55 +0200

    :bug: Fix Gallery round

--- palm_tracer/Processing/Gallery.py
+++ palm_tracer/Processing/Gallery.py
@@ -26,10 +26,30 @@ def make_gallery(stack: np.ndarray, localizations: pd.DataFrame, roi_size: int, 
 		pos_in_plane = idx % rois_per_plane				# Position dans la grille du plan
 		row, col = divmod(pos_in_plane, rois_per_line)  # Calculer la ligne et la colonne dans la grille
 
+		# Déterminer les bornes de la ROI (méthode non sure car avec les arrondis la ROI peut avoir une taille de roi_size ou roi_size + 1)
+		# half_size = roi_size / 2
+		# x_min, x_max = max(0, int(round(x - half_size))), min(max_width, int(round(x + half_size)))
+		# y_min, y_max = max(0, int(round(y - half_size))), min(max_height, int(round(y + half_size)))
+
 		# Déterminer les bornes de la ROI
-		half_size = roi_size / 2
-		x_min, x_max = max(0, int(round(x - half_size))), min(max_width, int(round(x + half_size)))
-		y_min, y_max = max(0, int(round(y - half_size))), min(max_height, int(round(y + half_size)))
+		half_size = roi_size // 2
+		# Calcul centre arrondi
+		x_center = int(round(x))
+		y_center = int(round(y))
+
+		# Bornes en X
+		x_min = max(0, x_center - half_size)
+		x_max = x_min + roi_size
+		if x_max > max_width:
+			x_max = max_width
+			x_min = x_max - roi_size
+
+		# Bornes en Y
+		y_min = max(0, y_center - half_size)
+		y_max = y_min + roi_size
+		if y_max > max_height:
+			y_max = max_height
+			y_min = y_max - roi_size
 
 		# Extraire la région de l'image originale et la copier dans la grille
 		roi = stack[plane - 1, y_min:y_max, x_min:x_max]
@@ -39,6 +59,6 @@ def make_gallery(stack: np.ndarray, localizations: pd.DataFrame, roi_size: int, 
 		gx_min, gx_max = col * roi_size, (col + 1) * roi_size
 
 		# Vérifier si la ROI extraite doit être remplie à une taille fixe (si proche des bords)
-		res[gallery_plane, gy_min:gy_max, gx_min:gx_max][:roi.shape[0], :roi.shape[1]] = roi
+		res[gallery_plane, gy_min:gy_max, gx_min:gx_max] = roi
 
 	return res
