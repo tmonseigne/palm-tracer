6fe433986dd8b95aeb71f839f9faf5cca24dd4eb
Thibaut <thibaut.monseigne@u-bordeaux.fr>
2026-06-17 10:27:21 +0200

    :white_check_mark: Test for ROi Layer

--- palm_tracer/_tests/test_UI_ViewerHR.py
+++ palm_tracer/_tests/test_UI_ViewerHR.py
@@ -4,7 +4,7 @@ import shutil
 from qtpy.QtCore import QCoreApplication, QEvent, Qt
 
 from palm_tracer._tests.Utils import *
-from palm_tracer.Settings.Types import BaseUIType, ButtonGroup
+from palm_tracer.Settings.Types import BaseUIType, ButtonGroup, CheckRangeInt
 from palm_tracer.UI import ViewerHRWidget
 
 INPUT_FILE = INPUT_DIR / "stack.tif"
@@ -207,3 +207,32 @@ def test_generate(make_napari_viewer, patched_napari_viewer, capsys, monkeypatch
 	w._generate()
 	lines = get_lines_output(capsys)
 	assert len(lines) == 0
+
+
+##################################################
+def test_widget_roi_filter_layer(make_napari_viewer, patched_napari_viewer, qtbot):
+	"""Test sur le dessin de la ROI."""
+	viewer = make_napari_viewer()  # Créer un viewer à l'aide de la fixture.
+	shutil.rmtree(OUTPUT_FOLDER, ignore_errors=True)
+	pt = PALMTracer()
+	add_basic_file(pt)
+	pt.process()  # Process Vide pour créer le dossier et un paramètre de base
+	shutil.copy2(INPUT_DIR / "localizations.csv", INPUT_DIR / "stack_PALM_Tracer" / f"localizations-{pt._timestamp}.csv")
+	shutil.copy2(INPUT_DIR / "tracking.csv", INPUT_DIR / "stack_PALM_Tracer" / f"tracking-{pt._timestamp}.csv")
+	shutil.copy2(INPUT_DIR / "beads.csv", INPUT_DIR / "stack_PALM_Tracer" / f"beads-{pt._timestamp}.csv")
+	pt.load()
+	pt.df["loc"]["Integrated Intensity"] *= 100
+	w = ViewerHRWidget(viewer, pt)  # Créer notre widget, en passant par le viewer.
+
+	filter_x = cast(CheckRangeInt, w._pt.settings.filters.localization["X"])
+	layers, l_name = w.viewer.layers, "ROI Filter"
+
+	w._add_roi_filter_layer()  # .										Lancement sans aucune entrée.
+	w._generate()  # .													Génération de la Vizualization.
+	qtbot.waitUntil(lambda: "Visualization" in layers, timeout=5000)  # Attente : qu'il ait mis une image.
+	filter_x.active = True  # .											On active le filtre sur X, mais il prend tout l'espace donc pas d'affichage
+	filter_x.min = 10  # .												On a un filtre qui est plus petit que l'image
+	qtbot.waitUntil(lambda: l_name in layers, timeout=5000)  # .		Attente : qu'il ait mis une image.
+	filter_x.min = 11  # .												On a un filtre qui est plus petit que l'image, mise à jour du calque
+	filter_x.active = False  # .										On désactive le filtre pour supprimer les calques
+	qtbot.waitUntil(lambda: l_name not in layers, timeout=5000)  # .	Attente : qu'il n'y ait plus d'une image.
